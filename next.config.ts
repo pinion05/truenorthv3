acecccb8115c021ff329ebafa18f05cbac1577a2
“pinion05” <shalompmc0505@naver.com>
2026-03-10 14:57:07 +0900

    chore: setup github pages basePath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = "";
+if (isGithubActions) {
+  const repoFullName = process.env.GITHUB_REPOSITORY || "";
+  repo = repoFullName.split("/")[1] || "";
+}
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: repo ? `/${repo}` : undefined,
   images: {
     unoptimized: true,
   },
